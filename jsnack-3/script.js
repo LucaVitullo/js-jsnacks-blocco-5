75cae7bd754db7ea32e8f46eb5e05a46518672b6
Lucavit9 <95365833+Lucavit9@users.noreply.github.com>
2022-03-22 18:16:01 +0100

    esercizio 3 fatto

--- jsnack-3/script.js
+++ jsnack-3/script.js
@@ -1,1 +1,14 @@
 //Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
+
+const parola= prompt('inserisci una parola');
+
+let parolaAlContrario='';
+
+for(let i = parola.length -1; i >=0 ; i--){
+
+     parolaAlContrario += parola[i];
+}
+
+console.log(parolaAlContrario);
+
+
